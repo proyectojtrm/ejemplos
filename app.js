3d28578f216ae628a00b2ebd278ded6e5bbe5a98
brigida-paredes <brigida.paredest@hotmail.com>
2016-06-08 21:31:54 -0400

    proyecto2

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express/partials');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', routes);
 app.use('/users', users);
